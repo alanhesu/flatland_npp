f0df8420a94bcb845142f1fd1c36a0a515c4d317
Alan Hesu <ahesu6@gatech.edu>
2022-03-28 23:20:19 -0400

    add ability to add multiple of the same object in map_to_model script

--- flatland_npp/scripts/map_to_model.py
+++ flatland_npp/scripts/map_to_model.py
@@ -29,19 +29,29 @@ def get_contours(img, model_list):
         # cv2.waitKey(0)
 
         cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)[0]
+        cnts = imutils.grab_contours(cnts)
         # convert to a numpy array
-        cnts = np.array(cnts)[:,0,:]
+        cnts = np.array(cnts)
+        cnts = np.reshape(cnts, (cnts.shape[0], cnts.shape[1], cnts.shape[-1]))
         cnts = cnts.astype('float64')
-        # add an extra pixel of padding
-        xind = np.where(cnts[:,0] != np.min(cnts[:,0]))
-        yind = np.where(cnts[:,1] != np.min(cnts[:,1]))
-        cnts[xind,0] = cnts[xind,0] + 1
-        cnts[yind,1] = cnts[yind,1] + 1
-        model_list[i].append(cnts)
 
-        # also add origin to list
-        orig = np.mean(cnts, axis=0)
+        # add an extra pixel of padding
+        for j in range(0, cnts.shape[0]):
+            shape = cnts[j,:,:]
+            xind = np.where(shape[:,0] != np.min(shape[:,0]))
+            yind = np.where(shape[:,1] != np.min(shape[:,1]))
+            shape[xind,0] = shape[xind,0] + 1
+            shape[yind,1] = shape[yind,1] + 1
+
+        # use first contour as object shape
+        shape = cnts[0,:,:]
+        model_list[i].append(shape)
+
+        # add origin of each instance to the list
+        orig = []
+        for j in range(0, cnts.shape[0]):
+            orig.append(np.mean(cnts[j,:,:], axis=0))
+
         model_list[i].append(orig)
 
         # cv2.imshow('img', cv2.circle(img, tuple(orig.astype(int)), radius=0, color=(0, 0, 255), thickness=1))
@@ -64,14 +74,15 @@ def populate_modellist(model_list):
     lines = []
     for model in model_list:
         modnum = 0
-        lines.append('  - name: {}{}\n'.format(model[0], modnum))
-        lines.append('    pose: [{}, {}, 0.0]\n'.format(model[5][0], model[5][1]))
-        lines.append('    model: {}.model.yaml\n'.format(model[0]))
-        modnum += 1
+        for origin in model[5]:
+            lines.append('  - name: {}{}\n'.format(model[0], modnum))
+            lines.append('    pose: [{}, {}, 0.0]\n'.format(origin[0], origin[1]))
+            lines.append('    model: {}.model.yaml\n'.format(model[0]))
+            modnum += 1
 
     return lines
 
-def generate_models(imfile, csvfile, outdir, resolution, origin):
+def generate_models(imfile, csvfile, outdir, worldfile, resolution, origin):
     model_list = []
     # read in csv to generate list of models corresponding to rgb values
     model_list = read_model_list(csvfile, model_list)
@@ -85,16 +96,17 @@ def generate_models(imfile, csvfile, outdir, resolution, origin):
 
     # convert contours to polygon points for flatland model
     for i in range(0, len(model_list)):
-        model_list[i][4] = (model_list[i][4] - model_list[i][5])*resolution
+        model_list[i][4] = (model_list[i][4] - model_list[i][5][0])*resolution
 
         # convert points to world coordinates
-        loc = model_list[i][5]
-        loc[0] = loc[0]*resolution + origin[0]
-        loc[1] = (h - loc[1])*resolution + origin[1]
+        for j in range(0, len(model_list[i][5])):
+            loc = model_list[i][5][j]
+            loc[0] = loc[0]*resolution + origin[0]
+            loc[1] = (h - loc[1])*resolution + origin[1]
 
     # generate flatland model files
     # read in a template file template.model.yaml
-    template_file = 'template.model.yaml'
+    template_file = os.path.join(os.path.dirname(__file__), 'template.model.yaml')
     for model in model_list:
         lines = populate_modelfile(template_file, model)
         model_file = os.path.join(outdir, model[0] + '.model.yaml')
@@ -102,18 +114,20 @@ def generate_models(imfile, csvfile, outdir, resolution, origin):
             f.writelines(lines)
 
     # generate model list
-    world_modellist = 'modellist.world.yaml'
+    # world_modellist = 'modellist.world.yaml'
     lines = populate_modellist(model_list)
-    modellist_file = os.path.join(outdir, world_modellist)
-    with open(modellist_file, 'w') as f:
+    # modellist_file = os.path.join(outdir, world_modellist)
+    with open(worldfile, 'a') as f:
+        f.write('# added from map_to_model script\n')
         f.writelines(lines)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('imfile')
 parser.add_argument('csvfile')
 parser.add_argument('outdir')
+parser.add_argument('worldfile')
 parser.add_argument('resolution', type=float, help='Resolution in meters/pixel. Should match map resolution')
 parser.add_argument('--origin', nargs=2, type=float, help='Origin of map in map.yaml')
 
 args = parser.parse_args()
-generate_models(args.imfile, args.csvfile, args.outdir, args.resolution, args.origin)
+generate_models(args.imfile, args.csvfile, args.outdir, args.worldfile, args.resolution, args.origin)
